2eeda8271e5b6080a261e8143ca9b8cce782b0cc
Mathivanan Manickam <mmanickam@ttsiglobal.com>
2019-04-30 10:29:33 -0400

    Draft

---
+++ mvnsample/src/main/java/com/freddiemac/gradle/mvnsample/MvnsampleApplication.java
@@ -0,0 +1,13 @@
+package com.freddiemac.gradle.mvnsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnsampleApplication.class, args);
+	}
+
+}
---
+++ mvnsample/src/test/java/com/freddiemac/gradle/mvnsample/MvnsampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.freddiemac.gradle.mvnsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvnsampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
